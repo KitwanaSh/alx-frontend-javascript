05990610f58616404ba5faec622b4c3eeba0f588
KitwanaSh <rodriguekitweze@gmail.com>
2023-04-20 14:38:53 +0200

    Feat: add the task #10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+export default class car {
+    constructor(brand, motor, color) {
+        if (typeof brand !== 'string'
+        || typeof motor !== 'string'
+        || typeof color !== 'string') throw new Error();
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    cloneCar() {
+        return new this.constructor(this._brand, this._motor, this._color);
+    }
+}
